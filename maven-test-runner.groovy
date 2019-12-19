64f8779021184b38e10609f8ca13bca24da99ff4
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2019-12-19 23:53:47 +0300

    Add running maven test

---
+++ maven-test-runner.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /root/.m2:/root/.m2'
+        }
+    }
+    stages {
+		stage('Checkout') {
+			git 'https://github.com/mefmor/maven-positive-test1.git'
+		}
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
+    }
+}
